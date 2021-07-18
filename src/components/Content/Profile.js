d9fb21af5137f49e1dbaa13d82a62d290c22f7e6
mr-oroz <oroz7307@gmail.com>
2021-07-18 14:19:25 +0600

    'first'

---
+++ src/components/Content/Profile.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <section className='content'>
+            <div>
+                <img src='https://vjoy.cc/wp-content/uploads/2019/07/1-1.jpg' />
+            </div>
+            <div>
+                ava + decription
+            </div>
+            <div>
+                my posts
+                <div>
+                    New posts
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Profile;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://cdn.logo.com/hotlink-ok/enterprise/eid_422203f0-477b-492b-9847-689feab1452a/logo-dark-2020.png' />
+        </header>
+    );
+};
+
+export default Header;
